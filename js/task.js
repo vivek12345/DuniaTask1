d25fadd2f13ef4856aa993c76baab6149552e4d3
vivek12345 <vivek.of.nayyar@gmail.com>
2015-02-27 20:26:13 +0530

    Comments Added

--- js/task.js
+++ js/task.js
@@ -4,24 +4,30 @@ var MyNewClass=(function()
 	{
 		this.registeredFunctions=0;
 		this.count=0;
+		
+		/*This function will register a new function with my object*/
 		this.register=function(newFunction)
 		{
 			this['function'+(++this.registeredFunctions)]=newFunction;
 
 		};
 
+		/*This will start the execution of all the registered functions*/
 		this.start=function()
 		{
 			for(var i=1;i<=this.registeredFunctions;i++)
 				this['function'+i]();
+			/*Call the callback function,if the registered functions do not execute within 2sec*/
 			setTimeout(this.callback,2000);
 		};
 
+		/*This callback will be called once all have finished execution or timeout occurs*/
 		this.callback=function()
 		{
 			alert("I am callback");
 		};
 
+		/*All registered function's on success call this method,to register there completion*/
 		this.markDone=function()
 		{
 			this.count++;
--- js/test.js
+++ js/test.js
@@ -1,3 +1,4 @@
+/*This is a test file,which woill create an instance of my object and start registering task's to it*/
 $(document).ready(function()
 {
 	$('.start').click(function(){
